b1741252c276d6a63fa4c2e2c8f87a4147979df9
istekhar <istekharkhan46@gmail.com>
2024-03-25 23:11:27 +0530

    dash board setup done

---
+++ roaDo-dashboard/src/components/SidebarSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarSection = () => {
+  return (
+    <div>SidebarSection</div>
+  )
+}
+
+export default SidebarSection
---
+++ roaDo-dashboard/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SidebarSection  from '../components/SidebarSection.tsx'
+import HeroSection from '../components/HeroSection.tsx'
+
+const Home = () => {
+  return (
+    <section>
+        <SidebarSection />
+        <HeroSection />
+    </section>
+  )
+}
+
+export default Home
